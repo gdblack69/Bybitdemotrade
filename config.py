e5f3bf25bfd132fbecb8decf1819a3509227d601
gdblack69 <geetasaaroj@gmail.com>
2024-12-02 16:26:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 api_key = "nVIxy5BMLnEhi8oxC4"
 api_secret = "feIJsrtiXmFc0p4ll61saTYE83ssXSfu0bgc"
 api_hash = "b2dd9f27560e445abfe8f395d84ac3cf"
-bot_username = "@TypespaceBot"
+bot_username = "@@gpt3_unlim_chatbot"
 api_id = 22936737
 phone_number = 917897293175
