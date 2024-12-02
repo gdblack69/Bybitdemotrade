f47e4cd19718507166fa8dedb76c24c9d0896be0
gdblack69 <geetasaaroj@gmail.com>
2024-12-02 16:10:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ api_hash = "b2dd9f27560e445abfe8f395d84ac3cf"
 bot_username = "@TypespaceBot"
 api_id = 22936737
 phone_number = 917897293175
+source_session_file = "newsessionfile.session"
+destination_session_file = "newdestinationsessionfile.session"
