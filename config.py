8033ef138c5a42ee79a5859e70ad14706791a11d
gdblack69 <geetasaaroj@gmail.com>
2024-12-02 13:48:20 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+api_key = "nVIxy5BMLnEhi8oxC4"
+api_secret = "feIJsrtiXmFc0p4ll61saTYE83ssXSfu0bgc"
+api_hash = "b2dd9f27560e445abfe8f395d84ac3cf"
+bot_username = "@gpt3_unlim_chatbot"
+api_id = 22936737
+phone_number = 917897293175
