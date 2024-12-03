128853d7ed25947a7c45e059ad869af5d2370c30
gdblack69 <geetasaaroj@gmail.com>
2024-12-03 16:14:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 api_key = "nVIxy5BMLnEhi8oxC4"
 api_secret = "feIJsrtiXmFc0p4ll61saTYE83ssXSfu0bgc"
-api_hash = "b2dd9f27560e445abfe8f395d84ac3cf"
+api_hash = "edbecdc187df07fddb10bcff89964a8e"
 bot_username = "@gpt3_unlim_chatbot"
-api_id = 22936737
+api_id = 14135677
 phone_number = 917897293175
